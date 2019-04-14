4ab548c942b74345efa254fbbf03a891cd60f04c
test11git1 <49588548+test11git1@users.noreply.github.com>
2019-04-14 16:59:37 +0800

    Update App.java

--- src/main/java/com/chenzhan/App.java
+++ src/main/java/com/chenzhan/App.java
@@ -14,6 +14,6 @@ public class App {
         System.out.println("hello");
         System.out.println("branch test 6666");
         System.out.println("branch test 22222");
-        System.out.println("branch test 4444");
+        System.out.println("branch test 8888");
     }
 }
