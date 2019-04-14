c37e0ad940a3d7dcb88203ac56624e5c24b541cf
chenzhan2_glb <chenzhan2_glb@yy.com>
2019-04-14 15:41:53 +0800

    branch test

--- src/main/java/com/chenzhan/App.java
+++ src/main/java/com/chenzhan/App.java
@@ -8,5 +8,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("git test 222");
         System.out.println("3333");
+        System.out.println("branch develop 1.4.9");
     }
 }
